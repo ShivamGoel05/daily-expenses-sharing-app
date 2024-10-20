04162db3fc5486c8b54940fd5ac96de111f71db8
Shivam Goel <110183387+ShivamGoel05@users.noreply.github.com>
2024-10-20 16:31:35 +0530

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,12 @@ const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
     const { email, name, mobile } = req.body;
+
+    // Validate user inputs
+    if (!email || !name || !mobile) {
+        return res.status(400).json({ message: 'Email, name, and mobile number are required.' });
+    }
+
     try {
         const user = new User({ email, name, mobile });
         await user.save();
